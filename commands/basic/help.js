f32fa05dbfaf617b7b6372628f1c4507538a394f
pial-nf <personalemail2106263@gmail.com>
2025-03-05 21:05:10 +0600

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -125,7 +125,7 @@ module.exports = {
                 pages.push({
                     title: 'Bot Information',
                     description: [
-                        `- **Developer:** GlaceYT`,
+                        `- **Developer:** Pial Mallik`,
                         `- **Version:** 1.2.1`,
                         `- **Commands Loaded:** ${totalCommandsLoaded}`,
                         `- **Master Commands:** ${masterCount}`,
